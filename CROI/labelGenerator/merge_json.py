55c703ffb5d9146ec5a2b651b0384a1c01e3258c
lixiongbiao <392914306@qq.com>
2024-09-27 23:20:18 +0800

    合并数据集和

--- CROI/labelGenerator/merge_json.py
+++ CROI/labelGenerator/merge_json.py
@@ -5,14 +5,26 @@
 
 import json
 
+# phase = 'train'
+phase = 'val'
+
 # 读取第一个 JSON 文件
-with open('../data/palmprint/annotations/Tongji_val.json', 'r') as f1:
+with open(f'../data/palmprint/annotations/Tongji_{phase}.json', 'r') as f1:
     json_data1 = json.load(f1)
+# for item in json_data1["images"]:
+#     item['file_name'] = item['file_name'][7:]
 
 # 读取第二个 JSON 文件
-with open('./Tongji_val2.json', 'r') as f2:
+# with open('./Tongji_val2.json', 'r') as f2:
+with open(f'../data/palmprint/annotations/MPD_{phase}.json', 'r') as f2:
     json_data2 = json.load(f2)
 
+first_batch_count =len( json_data1['images'])
+for item in json_data2["images"]:
+    item['id'] += first_batch_count
+for item in json_data2['annotations']:
+    item['id'] += first_batch_count
+    item['image_id'] = item['id']
 # 创建一个新的字典，合并两个 JSON 数据
 merged_data = {
     "images": json_data1["images"] + json_data2["images"],
@@ -21,5 +33,5 @@ merged_data = {
 }
 
 # 将合并后的字典写入新的 JSON 文件
-with open('merged_file.json', 'w') as outfile:
+with open(f'merged_{phase}.json', 'w') as outfile:
     json.dump(merged_data, outfile, indent=4)
